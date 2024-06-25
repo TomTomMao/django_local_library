74a587a98bb1c6df2c60f6ae53c43b3209fbf261
Wentao Mao <118118504+TomTomMao@users.noreply.github.com>
2024-06-25 14:07:25 +0800

    added book page and author page

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -129,7 +129,6 @@ class Author(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a particular author instance."""
         return reverse('author-detail', args=[str(self.id)])
-
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
