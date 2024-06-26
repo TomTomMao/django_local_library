480d6a1b6df5ce1ff31e3a3a8367804fbca6a37b
Wentao Mao <118118504+TomTomMao@users.noreply.github.com>
2024-06-26 16:23:03 +0800

    added forms for author and renew borrow

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -6,11 +6,19 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-    path('authors/<str:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('author/<str:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
 urlpatterns += [
     path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+]
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,20 +1,27 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Book, Author, BookInstance, Genre
+'''
+    Django View
+'''
+import datetime
+
+from catalog.forms import RenewBookForm
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.contrib.auth.decorators import login_required
+
+from .models import Author, Book, BookInstance
+
 
 @login_required
 def index(request):
     """View function for home page of site."""
-    
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instance = BookInstance.objects.all().count()
-    
     # Available books (status = 'a)
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     print('num_instances_available:', num_instances_available)
@@ -22,8 +29,6 @@ def index(request):
     num_authors = Author.objects.count()
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
-    
-    
     context = {
         'num_books': num_books,
         'num_instances': num_instance,
@@ -31,44 +36,80 @@ def index(request):
         'num_authors': num_authors,
         'num_visits': num_visits
     }
-    
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
-
 class BookListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view for a list of books."""
     model = Book
     # paginate_by = 3
-    
 class BookDetailView(LoginRequiredMixin, generic.DetailView):
     model = Book
-
 class AuthorListView(LoginRequiredMixin, generic.ListView):
-    model = Author
-    
+    model = Author   
 class AuthorDetailView(LoginRequiredMixin, generic.DetailView):
-    model = Author
-    
+    model = Author  
 class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
     model = BookInstance
     template_name = 'catalog/bookinstance_list_borrowed_user.html'
     paginate_by = 10
-    
     def get_queryset(self):
         return (
             BookInstance.objects.filter(borrower=self.request.user)
             .filter(status__exact='o')
             .order_by('due_back')
         )
-
 class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
     model = BookInstance
     template_name = 'catalog/bookinstance_list_borrowed_all.html'
     paginate_by = 10
     permission_required = 'catalog.can_mark_returned'
-    
     def get_queryset(self):
         return(
             BookInstance.objects.filter(status__exact='o').order_by('due_back')
         )
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def renew_book_librarian(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+    if request.method == 'POST':
+        form = RenewBookForm(request.POST)
+        if form.is_valid():
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+            return HttpResponseRedirect(reverse('all-borrowed'))
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+    return render(request, 'catalog/book_renew_librarian.html', context)
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/11/2023'}
+    permission_required = 'catalog.add_author'
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author
+    # Not recommended (potential security issue if more fields added)
+    fields = '__all__'
+    permission_required = 'catalog.change_author'
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+    permission_required = 'catalog.delete_author'
+
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.success_url)
+        except Exception as e:
+            return HttpResponseRedirect(
+                reverse("author-delete", kwargs={"pk": self.object.pk})
+            )
